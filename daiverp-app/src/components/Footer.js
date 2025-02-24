c78a481d0f0a6750a65c52a80a7f164e50d36e27
seanm <20094677@mail.wit.ie>
2025-02-24 15:40:13 +0000

    removed project landing page from footer.

--- daiverp-app/src/components/Footer.js
+++ daiverp-app/src/components/Footer.js
@@ -3,7 +3,6 @@ import React from "react";
 function Footer() {
   return (
     <footer style={{ textAlign: "center", padding: "20px", marginTop: "20px", background: "#f4f4f4" }}>
-      <p>Project Landing Page: <a href="https://SeanMercier.github.io/daiverp">View Here</a></p>
       <p>GitHub Repository: <a href="https://github.com/SeanMercier/daiverp-fyp-app.git">GitHub Repo</a></p>
     </footer>
   );
